ff636e13b52b4aa3f7f38337ac329ed6713c6d5a
Renato Novais <renatonovais.ps@gmail.com>
2026-02-20 20:18:07 -0300

    Resolvendo Exercícios

--- 01_Introducao_programacao_usando_python/Capitlo_02_Tipos_de_Dados/Exercício 2.1 Expressões e Operadores.py
+++ 01_Introducao_programacao_usando_python/Capitlo_02_Tipos_de_Dados/Exercício 2.1 Expressões e Operadores.py
@@ -9,16 +9,19 @@ mediaIdade = (23 + 19 + 31) /6
 print(mediaIdade)
 
 # O número de vezes que 73 cabe em 403.
-
+print("Para saber quantas vezes um ")
+print("O 73 cabe", 403 // 73, " vezes em 403")
 
 # O resto da divisão de 403 por 73.
 
+print("O resto da divisão de 403 por 73 é calculado usando o operador de %, e será iagual a:", 403 % 73)
 
-# 2 elevado à $10ª$ potência.
+# 2 elevado à 10ª potência.
 
-
+print(2**10)
 
 # O valor absoluto da diferença entre a altura de duas pessoas (ex: 54 e 57).
+
 
 
 # O menor preço entre uma lista de valores (ex: 34.99, 29.95 e 31.50).
